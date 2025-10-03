7444b13cc7ec15b183e1761fd4a187c14c09a046
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-10-03 17:10:43 +0000

    Refactor: track changes sum directly instead of managing a sliding window

--- first-problem.py
+++ first-problem.py
@@ -1,14 +1,14 @@
 def findMaxAverage(nums: list[int], k: int):
     # initial window and sum
-    window = nums[:k]
-    window_sum = sum(window)
+    window_sum = sum(nums[:k])
     max_sum = window_sum
 
     # slide window
     for i in range(k, len(nums)):
-        item_leaving = window.pop(0)
-        window.append(nums[i])
-        window_sum += nums[i] - item_leaving
+        window_sum += nums[i] - nums[i - k]
         max_sum = max(window_sum, max_sum)
 
     return max_sum / k
+
+
+print(findMaxAverage([2, 49, 29, 9, 10, 92, 12], 4))
